2245e981fd632f90e05b297116173219ab03991c
Serj Sidelnikov <sv@spiks.ru>
2020-07-01 16:04:17 +0300

    The logic of films downloads

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -1,11 +1,13 @@
 import films from '../mocks/films';
 import reviews from '../mocks/reviews';
-import {ALL_GENRES} from '../const';
+import {ALL_GENRES, MAX_SHOW_FILM} from '../const';
 
 const initialState = {
   currentGenre: ALL_GENRES,
   genres: [...new Set(films.map((film) => film.genre))],
   films,
+  chunkFilms: [],
+  isMoreFilms: films.length > MAX_SHOW_FILM,
   reviews,
   promoItem: {
     title: `The Grand Budapest Hotel`,
@@ -14,9 +16,13 @@ const initialState = {
   }
 };
 
+window.state = initialState;
+
 const ActionType = {
   CHANGE_GENRE_FILTER: `CHANGE_GENRE_FILTER`,
   GET_FILTERED_FILMS: `GET_FILTERED_FILMS`,
+  LOAD_FILMS: `LOAD_FILMS`,
+  SHOW_MORE: `SHOW_MORE`,
 };
 
 const ActionCreator = {
@@ -37,6 +43,14 @@ const ActionCreator = {
       payload: filteredFilms,
     };
   },
+
+  loadFilms: () => ({
+    type: ActionType.LOAD_FILMS,
+  }),
+
+  showMore: () => ({
+    type: ActionType.SHOW_MORE,
+  }),
 };
 
 const reducer = (state = initialState, action) => {
@@ -49,6 +63,19 @@ const reducer = (state = initialState, action) => {
     case ActionType.GET_FILTERED_FILMS:
       return Object.assign({}, state, {
         films: action.payload,
+      });
+
+    case ActionType.LOAD_FILMS:
+      return Object.assign({}, state, {
+        chunkFilms: state.films.slice(0, MAX_SHOW_FILM),
+        isMoreFilms: state.films.length > MAX_SHOW_FILM,
+      });
+
+    case ActionType.SHOW_MORE:
+      const newChunk = state.films.slice(state.chunkFilms.length, state.chunkFilms.length + MAX_SHOW_FILM);
+      return Object.assign({}, state, {
+        chunkFilms: state.chunkFilms.concat(newChunk),
+        isMoreFilms: newChunk.length === MAX_SHOW_FILM,
       });
 
     default:
